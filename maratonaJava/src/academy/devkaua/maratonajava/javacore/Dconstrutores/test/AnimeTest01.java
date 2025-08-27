5ef3ec35e7bd9991b92b7552c1470cf896f2d4d2
Kaua Henrique <kaua.h@academico.ifrn.edu.br>
2025-08-27 11:47:33 -0300

    Adcionando conteudo "Construtores"

--- maratonaJava/src/academy/devkaua/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ maratonaJava/src/academy/devkaua/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,7 +4,7 @@ import academy.devkaua.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Goku","TV",12,"Aventura");
+        Anime anime = new Anime("Goku","TV",12,"Aventura", "Reanimeiton");
 
         anime.imprimir();
     }
