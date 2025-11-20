e0eebe3ecc83e43fdfc7093baf5ac1f82890f5b4
Kaua-Henrique1 <kaua2006henrique023@gmail.com>
2025-11-20 09:07:22 -0300

    Revisao geral

--- maratonaJava/src/academy/devkaua/maratonajava/javacore/Gassociacao/domain/Time.java
+++ maratonaJava/src/academy/devkaua/maratonajava/javacore/Gassociacao/domain/Time.java
@@ -2,9 +2,23 @@ package academy.devkaua.maratonajava.javacore.Gassociacao.domain;
 
 public class Time {
     private String nome;
+    private jogador[] jogadores;
 
     public Time(String nome) {
         this.nome = nome;
+    }
+
+    public Time(String nome, jogador[] jogadores) {
+        this.nome = nome;
+        this.jogadores = jogadores;
+    }
+
+    public void imprimi() {
+        System.out.println(this.nome);
+        if (this.jogadores == null) return;
+        for (String jogadores : jogador) {
+            System.out.println(this.nome);
+        }
     }
 
     public String getNome() {
